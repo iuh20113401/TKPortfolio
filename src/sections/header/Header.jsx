cbe352e8852d1d4da92c2e07a454e52f7cb5955d
Nguyễn Tuấn Kiệt <kiet5454aa@gmail.com>
2025-01-11 10:50:11 +0700

    update soem information

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react'
-import HeaderImage from '../../assets/header.jpg'
-import data from './data'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import './header.css'
+import { useEffect } from "react";
+import HeaderImage from "../../assets/header.jpg";
+import data from "./data";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./header.css";
 
 const Header = () => {
   useEffect(() => {
-    AOS.init({duration: 2000})
-  }, [])
+    AOS.init({ duration: 2000 });
+  }, []);
 
   return (
     <header id="header">
@@ -16,22 +16,35 @@ const Header = () => {
         <div className="header__profile" data-aos="fade-in">
           <img src={HeaderImage} alt="Header Portait" />
         </div>
-        <h3  data-aos="fade-up">Hajia Bintu</h3>
-        <p  data-aos="fade-up">
-        You are a click away from building your dream website or web app. Send me the details of your project for a modern, mobile responsive, highly performant website today!
+        <h3 data-aos="fade-up">Tuan Kiet Nguyen </h3>
+        <p data-aos="fade-up">
+          You are a click away from building your dream website or web app. Send
+          me the details of your project for a modern, mobile responsive, highly
+          performant website today!
         </p>
-        <div className="header__cta"  data-aos="fade-up">
-          <a href="#contact" className='btn primary'>Let's Talk</a>
-          <a href="#portfolio" className='btn light'>My Work</a>
+        <div className="header__cta" data-aos="fade-up">
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
         </div>
         <div className="header__socials">
-          {
-            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
-          }
+          {data.map((item) => (
+            <a
+              key={item.id}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
